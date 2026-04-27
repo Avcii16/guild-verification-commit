2e4a9a115bdfeb7d2208c5ba6e7489ebdfaebee8
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-28 00:41:15 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-27T18:15:26.786Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-27T21:41:14.956Z
+Commit: Patch API endpoint error handling

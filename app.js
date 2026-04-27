52b44b00ec8fc59bb645da114cb4072309e8e04f
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-27 23:13:56 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-27T19:36:37.200Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-27T20:13:56.863Z
+Commit: Fix broken links in documentation

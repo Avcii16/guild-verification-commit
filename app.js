571a9460b9c74396c6cdb639b7ca38322ee2f8fd
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-27 22:36:37 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-27T18:15:18.571Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-27T19:36:37.200Z
+Commit: Fix CSS styling bug on mobile devices

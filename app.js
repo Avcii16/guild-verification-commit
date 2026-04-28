64c15c8c89196f86eb45f0b114c2f4d9b09aaf5b
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-28 21:27:11 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-28T18:19:38.308Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-28T18:27:12.660Z
+Commit: Resolve null pointer exception in user service

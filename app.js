5c896eac30fd55d594af472e6d600b5c6da123a0
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-29 00:36:29 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-28T21:13:04.093Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-28T21:36:30.603Z
+Commit: Resolve null pointer exception in user service

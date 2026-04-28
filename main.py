5282fbd64ea68cb1351987e6cdd867dcbf158c34
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-29 01:45:53 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-28T22:44:29.244Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-28T22:45:54.258Z
+Commit: Patch API endpoint error handling

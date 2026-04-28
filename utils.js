e45940105c9002b99441ac0f5689c6e52b546cbc
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-29 00:59:12 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-28T21:53:09.553Z
-Commit: Add multi-language support
+Timestamp: 2026-04-28T21:59:13.367Z
+Commit: Resolve null pointer exception in user service

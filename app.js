a77abfa2c9d1709ca45346b7bebbe13a1173f951
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-29 00:53:10 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-28T21:36:30.603Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-28T21:53:11.417Z
+Commit: Fix broken links in documentation

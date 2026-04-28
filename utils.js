5d46d45c98a977ad469ce04297f6e6195af26e4b
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-29 01:10:16 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-28T22:10:16.481Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-28T22:10:17.649Z
+Commit: Patch API endpoint error handling

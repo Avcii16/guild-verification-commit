bbd19df8cf26d60f38db36a278a81da0c8aa6aa8
Avcii16 <164826951+Avcii16@users.noreply.github.com>
2026-04-29 01:57:33 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-28T22:53:24.494Z
-Commit: Add dark mode support
+Timestamp: 2026-04-28T22:57:34.850Z
+Commit: Resolve null pointer exception in user service
